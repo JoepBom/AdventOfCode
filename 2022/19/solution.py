4f4933eb707e82f8cae41dcc3492b04142ba7dc6
JBomNotilyze <joep.bom@notilyze.com>
2023-08-22 13:34:06 +0200

    added solution to day 19

--- 2022/19/solution.py
+++ 2022/19/solution.py
@@ -1,8 +1,8 @@
 import numpy as np
 import re
 from pprint import pprint
-
-f = open("C:/Users/joep_/Documents/AdventOfCode/2022/19/input_small", "r")
+import random
+f = open("C:/Users/JoepBom/Documents/AdventOfCode/2022/19/input", "r")
 input = f.read().splitlines()
 class Blueprint:
     number: int
@@ -18,71 +18,99 @@ class Blueprint:
         self.obsidian_robot={"ore": numbers[2], "clay": numbers[3]}
         self.geode_robot={"ore": numbers[4], "obsidian": numbers[5]}
     
+    def get_max_ore(self):
+        return max(self.ore_robot["ore"], self.clay_robot["ore"], self.obsidian_robot["ore"], self.geode_robot["ore"])
+    
+
 blueprints=[]
 for nr, i in enumerate(input, 1):
     j = i.split(" ")
     blueprints.append(Blueprint(nr, [int(j[6]), int(j[12]), int(j[18]), int(j[21]), int(j[27]), int(j[30])]))
 
 
-
-
-def compare_states(state1, state2):
-    state1_win = True
-    state2_win = True
-    for key in state1.keys():
-        if key in {"time_left", "buying"}:
-            continue
-        if state1[key]> state2[key]:
-            state2_win = False
-        if state1[key]< state2[key]:
-            state1_win = False
-    if state1_win == state2_win:
-        return None
-    if state1_win:
-        return 0
-    if state2_win:
-        return 1
-
-def get_possible_states(bp: Blueprint, state: dict):
+def get_next_states(bp: Blueprint, state: dict):
     new_states = []
-    if state["ore"]>= bp.ore_robot["ore"]:
-        res = state.copy()
-        res["buying"]="ore_robots"
+    res = state.copy()
+    res["time_left"]-=1
+    res["ore"]+=res["ore_robots"]
+    res["clay"]+=res["clay_robots"]
+    res["obsidian"]+=res["obsidian_robots"]
+    res["geode"]+=res["geode_robots"]
+    if state["next_to_buy"]=="ore_robots" and state["ore"]>= bp.ore_robot["ore"]:
         res["ore"]-=bp.ore_robot["ore"]
-        new_states.append(res)
-    if state["ore"]>= bp.clay_robot["ore"]:
-        res = state.copy()
-        res["buying"]="clay_robots"
+        res["ore_robots"]+=1
+        if res["ore_robots"]<bp.get_max_ore():
+            res["next_to_buy"]="ore_robots"
+            new_states.append(res)
+        res1=res.copy()
+        res1["next_to_buy"]="clay_robots"
+        new_states.append(res1)
+        res2=res.copy()
+        if res2["clay_robots"]>0:
+            res2["next_to_buy"]="obsidian_robots"
+            new_states.append(res2)
+        res3=res.copy()
+        if res3["obsidian_robots"]>0:
+            res3["next_to_buy"]="geode_robots"
+            new_states.append(res3)
+        return new_states
+    if state["next_to_buy"]=="clay_robots" and state["ore"]>= bp.clay_robot["ore"]:
         res["ore"]-=bp.clay_robot["ore"]
-        new_states.append(res)    
-    if state["ore"]>= bp.obsidian_robot["ore"] and state["clay"]>= bp.obsidian_robot["clay"]:
-        res = state.copy()
-        res["buying"]="obsidian_robots"
+        res["clay_robots"]+=1
+        if res["ore_robots"]<bp.get_max_ore():
+            res["next_to_buy"]="ore_robots"
+            new_states.append(res)
+        res0=res.copy()
+        res0["next_to_buy"]="clay_robots"
+        new_states.append(res0)
+        res1=res.copy()
+        res1["next_to_buy"]="obsidian_robots"
+        new_states.append(res1)
+        res2=res.copy()
+        if res2["obsidian_robots"]>0:
+            res2["next_to_buy"]="geode_robots"
+            new_states.append(res2)
+        return new_states
+    if state["next_to_buy"]=="obsidian_robots" and state["ore"]>= bp.obsidian_robot["ore"] and state["clay"]>= bp.obsidian_robot["clay"]:
         res["ore"]-=bp.obsidian_robot["ore"]
         res["clay"]-=bp.obsidian_robot["clay"]
-        new_states.append(res) 
-    if state["ore"]>= bp.geode_robot["ore"] and state["obsidian"]>= bp.geode_robot["obsidian"]:
-        res = state.copy()
-        res["buying"]="geode_robots"
+        res["obsidian_robots"]+=1
+        if res["ore_robots"]<bp.get_max_ore():
+            res["next_to_buy"]="ore_robots"
+            new_states.append(res)
+        res0=res.copy()
+        res0["next_to_buy"]="clay_robots"
+        new_states.append(res0)
+        res1=res.copy()
+        res1["next_to_buy"]="obsidian_robots"
+        new_states.append(res1)
+        res2=res.copy()
+        res2["next_to_buy"]="geode_robots"
+        new_states.append(res2)
+        return new_states
+    if state["next_to_buy"]=="geode_robots" and state["ore"]>= bp.geode_robot["ore"] and state["obsidian"]>= bp.geode_robot["obsidian"]:
         res["ore"]-=bp.geode_robot["ore"]
         res["obsidian"]-=bp.geode_robot["obsidian"]
-        new_states.append(res)
-    res = state.copy()
-    res["buying"]=None
-    new_states.append(res)    
-    for i in new_states:
-        i["time_left"]-=1
-        i["ore"]+=i["ore_robots"]
-        i["clay"]+=i["clay_robots"]
-        i["obsidian"]+=i["obsidian_robots"]
-        i["geode"]+=i["geode_robots"]
-        if i["buying"] is not None:
-            i[i["buying"]]+=1
-    return new_states
+        res["geode_robots"]+=1
+        if res["ore_robots"]<bp.get_max_ore():
+            res["next_to_buy"]="ore_robots"
+            new_states.append(res)
+        res0=res.copy()
+        res0["next_to_buy"]="clay_robots"
+        new_states.append(res0)
+        res1=res.copy()
+        res1["next_to_buy"]="obsidian_robots"
+        new_states.append(res1)
+        res2=res.copy()
+        res2["next_to_buy"]="geode_robots"
+        new_states.append(res2)
+        return new_states
+    return [res]
+
 
 def evaluate_blueprint(bp: Blueprint):
-    initial_state = {
-        "time_left": 24,
+    initial_state1 = {
+        "time_left": 32,
         "ore_robots": 1,
         "clay_robots": 0,
         "obsidian_robots":0,
@@ -91,36 +119,37 @@ def evaluate_blueprint(bp: Blueprint):
         "clay": 0,
         "obsidian": 0,
         "geode": 0,
-        "buying": None
+        "next_to_buy": "ore_robots"
         }
-    states=[initial_state]
+    initial_state2 = {
+        "time_left": 32,
+        "ore_robots": 1,
+        "clay_robots": 0,
+        "obsidian_robots":0,
+        "geode_robots": 0,
+        "ore": 0,
+        "clay": 0,
+        "obsidian": 0,
+        "geode": 0,
+        "next_to_buy": "clay_robots"
+        }
+    states=[initial_state1, initial_state2]
     final_states = []
-    while len(states)>0:
-        print(len(states))
+    while states[0]["time_left"]>0:
+        print(states[0]["time_left"], len(states))
         new_states = []
         for i in states:
-            if i["time_left"]==0:
-                final_states.append(i)
-                continue
-            res=get_possible_states(bp, i)
+            res=get_next_states(bp, i)
             for j in res:
-                to_remove=[]
-                add = True
-                if len(new_states)<2000:
-                    for k in new_states:
-                        res=compare_states(j,k)
-                        if res is None:
-                            continue
-                        if res==1:
-                            add = False
-                            break
-                        if res==0:
-                            to_remove.append(k)
-                    for k in to_remove:
-                        new_states.remove(k)
-                if add:
-                    new_states.append(j)
-        states = new_states
+                new_states.append(j)
+        max_geode_potential = max(state["geode"]+state["time_left"]*state["geode_robots"] for state in new_states)
+        states=[]
+        for state in new_states:
+            if state["geode"]+state["time_left"]*state["geode_robots"]+ int(state["time_left"]**2/2)>=max_geode_potential:
+                states.append(state)
+        
+        # pprint(states)
+        
     print("done")
     best_state={
         "time_left": 0,
@@ -131,25 +160,17 @@ def evaluate_blueprint(bp: Blueprint):
         "ore": 0,
         "clay": 0,
         "obsidian": 0,
-        "geode": 0,
-        "buying": None
+        "geode": 0
         }
-    for i in final_states:
+    for i in states:
         if i["geode"]> best_state["geode"]:
             best_state = i
     print(best_state)
-    return best_state["geode"]*bp.number
-
-print(f"Final answer: {sum(evaluate_blueprint(i) for i in blueprints)}")
-
-
-
-
-
-
-
-
-
-
-
+    return best_state["geode"]
+prod = 1
+for count, i in enumerate(blueprints):
+    if count<3:
+        prod*=evaluate_blueprint(i)
+    print(count)
+print(f"Final answer: {prod}")
 
